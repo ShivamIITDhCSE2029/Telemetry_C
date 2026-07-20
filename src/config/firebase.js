24da65b344222bb6c4d9bfaa7114320d97fce788
Shivam Tripathi <cs25bt063@iitdh.ac.in>
2026-07-20 23:41:51 +0530

    Updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,23 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCU7LIFlEg6Yf66OxSEVZHrK9EXzwOPyGw",
-  authDomain: "telemetry-backend-5425a.firebaseapp.com",
-  projectId: "telemetry-backend-5425a",
-  storageBucket: "telemetry-backend-5425a.firebasestorage.app",
-  messagingSenderId: "290397446862",
-  appId: "1:290397446862:web:88c8b1d122dd319c1d269f",
-  databaseURL: "https://telemetry-backend-5425a-default-rtdb.firebaseio.com/"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 import { getDatabase } from "firebase/database";
 
-const db = getDatabase(app);
-export { db };
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
